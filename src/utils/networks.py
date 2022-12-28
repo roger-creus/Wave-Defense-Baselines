a42868baa4b1209188b4d08a1bb9ba82fa96b93f
roger-creus <creus99@gmail.com>
2022-12-27 23:10:52 -0500

    clean

--- src/utils/networks.py
+++ src/utils/networks.py
@@ -6,7 +6,6 @@ import os
 import sys
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 from utils.utils import *
-from utils.smirl import *
 
 from IPython import embed
 
@@ -52,90 +51,6 @@ class Agent(nn.Module):
             action = probs.sample()
         return action, probs.log_prob(action), probs.entropy(), self.critic(hidden)
         
-class Agent_SMIRL(nn.Module):
-    def __init__(self, envs, latent_dim = 128):
-        super().__init__()
-
-        self.latent_dim = latent_dim
-        
-        self.vae = VAE(in_channels = 3, latent_dim = self.latent_dim)
-
-        # input to smirl policy is z, mu(z), sigma(z) where mus and sigmas are computed episodic-like
-        self.actor = layer_init(nn.Linear(self.latent_dim * 3, envs.single_action_space.n), std=0.01)
-        self.critic = layer_init(nn.Linear(self.latent_dim * 3, 1), std=1)
-
-    def get_value(self, x, ep_mu, ep_sigma):
-        mu, logvar = self.vae.encode(x / 255.0)
-        z = self.vae.reparameterize(mu, logvar)
-
-        x = torch.cat([z, ep_mu, ep_sigma], axis = 1)
-        
-        return self.critic(x)
-
-
-    def get_action_and_value(self, x, ep_mu, ep_sigma, action=None):
-        mu, logvar = self.vae.encode(x / 255.0)
-        z = self.vae.reparameterize(mu, logvar)
-
-        x = torch.cat([z, ep_mu, ep_sigma], axis = 1)
-
-        logits = self.actor(x)
-        probs = Categorical(logits=logits)
-        if action is None:
-            action = probs.sample()
-        return action, probs.log_prob(action), probs.entropy(), self.critic(x)
-
-class Agent_RND(nn.Module):
-    def __init__(self, envs):
-        super().__init__()
-        self.network = nn.Sequential(
-            layer_init(nn.Conv2d(4, 32, 8, stride=4)),
-            nn.ReLU(),
-            layer_init(nn.Conv2d(32, 64, 4, stride=2)),
-            nn.ReLU(),
-            layer_init(nn.Conv2d(64, 64, 3, stride=1)),
-            nn.ReLU(),
-        )
-
-        self.fc = nn.Sequential(
-            nn.Flatten(),
-            layer_init(nn.Linear(64 * 7 * 7, 256)),
-            nn.ReLU(),
-            layer_init(nn.Linear(256, 448)),
-            nn.ReLU(),
-        )
-
-        self.extra_layer = nn.Sequential(layer_init(nn.Linear(448, 448), std=0.1), nn.ReLU())
-        
-        self.actor = nn.Sequential(
-            layer_init(nn.Linear(448, 448), std=0.01),
-            nn.ReLU(),
-            layer_init(nn.Linear(448, envs.single_action_space.n), std=0.01),
-        )
-        self.critic_ext = layer_init(nn.Linear(448, 1), std=0.01)
-        self.critic_int = layer_init(nn.Linear(448, 1), std=0.01)
-
-    def get_action_and_value(self, x, action=None):
-        hidden = self.fc(self.network(x / 255.0))
-        logits = self.actor(hidden)
-        probs = Categorical(logits=logits)
-        features = self.extra_layer(hidden)
-        if action is None:
-            action = probs.sample()
-        return (
-            action,
-            probs.log_prob(action),
-            probs.entropy(),
-            self.critic_ext(features + hidden),
-            self.critic_int(features + hidden),
-        )
-
-    def get_value(self, x):
-        hidden = self.fc(self.network(x / 255.0))
-        features = self.extra_layer(hidden)
-        return self.critic_ext(features + hidden), self.critic_int(features + hidden)
-
-
 class Agent_MLP(nn.Module):
     def __init__(self, envs):
         super().__init__()
